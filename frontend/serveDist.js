daab36d46654baae0c22b06a81df43e84fc328dc
Sevy Lortie <severn.lortie@gmail.com>
2020-08-07 23:53:38 -0400

    redirected all routes to index in express server

--- frontend/serveDist.js
+++ frontend/serveDist.js
@@ -1,4 +1,12 @@
 const express = require('express');
+const path = require('path');
 const app = express();
+
 app.use(express.static('./dist/app'));
+
+// redirect all requests to index.html
+app.get('*', (req, res) => {
+    res.sendFile(path.resolve(__dirname, 'dist', 'app', 'index.html'))
+})
+
 app.listen(8081);
